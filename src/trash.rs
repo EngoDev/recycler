3d90d2a02c4b07679986f94b07702d428e78ee7b
EngoDev <shalev.goo@gmail.com>
2023-12-05 10:26:21 +0200

    Changed trash to happen periodically

--- src/trash.rs
+++ src/trash.rs
@@ -7,6 +7,7 @@ use rand::Rng;
 
 pub struct TrashPlugin;
 
+#[derive(Clone, Debug)]
 pub enum TrashType {
     Bottle,
     Pizza
@@ -18,60 +19,67 @@ pub struct Trash {
     pub trash_type: TrashType,
 }
 
+#[derive(Resource)]
+struct TrashSpawnTimer(Timer);
+
 /// This plugin handles player related stuff like movement
 /// Player logic is only active during the State `GameState::Playing`
 impl Plugin for TrashPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(TrashSpawnTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
         // app.add_systems(OnEnter(GameState::Playing), spawn_player)
-        app.add_systems(Update, spawn_trash.run_if(in_state(GameState::Playing)));
+        .add_systems(Update, spawn_trash.run_if(in_state(GameState::Playing)));
     }
 }
 
 
-fn spawn_trash(mut commands: Commands, textures: Res<TextureAssets>, keyboard_input: Res<Input<KeyCode>>, window: Query<&Window>) {
-    //     match self {
-    //         GameControl::Up => {
-    //             keyboard_input.pressed(KeyCode::W) || keyboard_input.pressed(KeyCode::Up)
-    //         }
-    //         GameControl::Down => {
-    //             keyboard_input.pressed(KeyCode::S) || keyboard_input.pressed(KeyCode::Down)
-    //         }
-    //         GameControl::Left => {
-    //             keyboard_input.pressed(KeyCode::A) || keyboard_input.pressed(KeyCode::Left)
-    //         }
-    //         GameControl::Right => {
-    //             keyboard_input.pressed(KeyCode::D) || keyboard_input.pressed(KeyCode::Right)
-    //         }
+fn spawn_trash(
+    mut commands: Commands,
+    textures: Res<TextureAssets>,
+    // keyboard_input: Res<Input<KeyCode>>,
+    window: Query<&Window>,
+    time: Res<Time>,
+    mut spawn_timer: ResMut<TrashSpawnTimer>,
+) {
+
+    static SPAWN_CHANCES: [TrashType; 2] = [TrashType::Bottle, TrashType::Pizza];
+
+    if spawn_timer.0.tick(time.delta()).just_finished() {
+        let window = window.single();
+
+        let mut random = rand::thread_rng();
+        let max_x: f32 = window.width() / 2.0;
+        let y_pos = window.height() / 2.0;
+
+        let random_x: f32 = random.gen_range(-max_x .. max_x);
+        let trash_type: TrashType = SPAWN_CHANCES[random.gen_range(0..SPAWN_CHANCES.len())].clone();
+
+        let trash = Trash {
+            trash_type
+        };
+        create_trash(&mut commands, &textures, trash, Vec2::new(random_x as f32, y_pos));
+    }
+
+
+    // match keyboard_input.pressed(KeyCode::B) {
+    //     true => {
+    //         let trash = Trash {
+    //             trash_type: TrashType::Bottle
+    //         };
+    //         create_trash(&mut commands, &textures, trash, Vec2::new(random_x as f32, y_pos));
     //     }
+    //     false => {}
     // }
-
-    let window = window.single();
-
-    let mut random = rand::thread_rng();
-    let max_x: f32 = window.width() / 2.0;
-    let y_pos = window.height() / 2.0;
-
-    let random_x: f32 = random.gen_range(-max_x .. max_x);
-
-    match keyboard_input.pressed(KeyCode::B) {
-        true => {
-            let trash = Trash {
-                trash_type: TrashType::Bottle
-            };
-            create_trash(&mut commands, &textures, trash, Vec2::new(random_x as f32, y_pos));
-        }
-        false => {}
-    }
-
-    match keyboard_input.pressed(KeyCode::P) {
-        true => {
-            let trash = Trash {
-                trash_type: TrashType::Pizza
-            };
-            create_trash(&mut commands, &textures, trash, Vec2::new(random_x as f32, y_pos));
-        }
-        false => {}
-    }
+    //
+    // match keyboard_input.pressed(KeyCode::P) {
+    //     true => {
+    //         let trash = Trash {
+    //             trash_type: TrashType::Pizza
+    //         };
+    //         create_trash(&mut commands, &textures, trash, Vec2::new(random_x as f32, y_pos));
+    //     }
+    //     false => {}
+    // }
 
 }
 
@@ -88,7 +96,6 @@ pub fn create_trash(commands: &mut Commands, textures: &Res<TextureAssets>, tras
         .insert(RigidBody::Dynamic)
         .insert(Collider::cuboid(0.5, 0.5))
         .insert(ColliderMassProperties::Mass(1.0))
-        // .insert(GravityScale(1.0))
         .insert(trash);
 }
 
