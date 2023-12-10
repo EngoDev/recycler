d5084fbe762129f90c9180eed208ee1fc3ea30c1
EngoDev <shalev.goo@gmail.com>
2023-12-10 23:43:21 +0200

    Submit version

--- src/menu.rs
+++ src/menu.rs
@@ -157,7 +157,7 @@ fn setup_menu(mut commands: Commands, textures: Res<TextureAssets>, window: Quer
                         normal: Color::NONE,
                         hovered: Color::rgb(0.25, 0.25, 0.25),
                     },
-                    OpenLink("https://github.com/NiklasEi/bevy_game_template"),
+                    OpenLink("https://github.com/EngoDev/recycler/"),
                 ))
                 .with_children(|parent| {
                     parent.spawn(TextBundle::from_section(
